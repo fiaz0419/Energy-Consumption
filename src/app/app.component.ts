2c680393ed7b5bcfc93e679b0e7b7f976498a93d
fayaz <fayaz.khader@clearpeaks.com>
2017-07-01 12:34:51 +0400

    first page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component ,OnInit} from '@angular/core';
+declare var $ : any;
+
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit{
+  title = 'Fayaz';
+  constructor(){ }
+
+  ngOnInit() {
+    console.log('hi');
+    $('.cbtn').click(function(){
+       console.log('cliked');
+    })
+  }
+
+
+  onButtonClick(){
+    this.title = 'Afsha';
+  }
 }
+
